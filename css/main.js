f85c0ef76e61c35dd45cecf732e4e9ecd40c8683
nome <marivaldocarmo2018@email.com>
2021-07-28 16:56:21 -0300

    obpc

---
+++ css/main.js
@@ -0,0 +1,80 @@
+//var nome="Pastor Marcio";
+//var n1=5;
+//var n2=3;
+//ar frase="Japão é o melhor time do mundo";
+//alert(nome + ' Tem ' + idade + ' anos');
+
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toUpperCase());          //Maiusculo
+//console.log(frase.toLowerCase());           //Minusculo
+//alert(frase.replace("Japão", "Brasil"));
+
+
+//var lista=['Maçã', 'Pera', 'Laranja'];
+//lista.push('Manga');
+
+//console.log(lista.toString());  //Acrecenta no final
+//console.log(lista.join('-'));   //Acrecenta traço
+
+/*var idade = prompt('Qual sua Idade?');
+//var idade = 18;
+if (idade >= 18) {
+    alert('Maior de Idade');
+}else{
+    alert('Menor de Idade');
+};*/
+
+/*var count = 0;
+while (count <=5){
+    console.log(count);
+    count = count + 1;
+};*/
+
+//var d = new Date();
+//alert(d);
+//alert(d.getDate);
+//alert(d.getMinutes);
+//alert(d.getUTCHours);
+
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+         validar= false
+    }
+    return validar;
+}
+var idade = prompt('Qual sua Idade');
+console.log(validaIdade(idade));*/
+
+function clicou(){
+    document.getElementById('Agradecimento').innerHTML = 'Obrigado Por Clicar';
+    //console.log(document.getElementById('Agradecimento').innerHTML + 'Obrigado Por Clicar');
+    //alert('Obrigado Por se Inscrever');
+}
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
